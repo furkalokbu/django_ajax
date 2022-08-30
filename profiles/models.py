dc55009206503bccb84cba4d882efaa09799d218
Serhii <furkalokbu@gmail.com>
2022-08-30 13:03:09 +0300

    add project to repository

---
+++ profiles/models.py
@@ -0,0 +1,17 @@
+from distutils.command.upload import upload
+from operator import mod
+from statistics import mode
+from turtle import update
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(default="avatar.png", upload_to='avatars')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"profile of the user {self.user.username}"
