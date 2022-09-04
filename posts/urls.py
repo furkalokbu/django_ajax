143bbad54be4a6fdd07707d34bfa9a90f7ccad61
Serhii <furkalokbu@gmail.com>
2022-09-04 21:01:59 +0300

    add posts and load avatar to profile

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    load_post_data_view,
+    like_unlike_post,
+    post_detail_view,
+    post_detail_data_view,
+    update_post_view,
+    delete_post_view,
+    image_upload_view
 )
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', post_list_and_create, name='main-board')
+    path('', post_list_and_create, name='main-board'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('upload/', image_upload_view, name='upload'),
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/', post_detail_view, name='post-detail'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+    path('<pk>/update/', update_post_view, name='post-update'),
+    path('<pk>/delete/', delete_post_view, name='post-delete'),
 ]
